9058f0f9ea71152b5c36c358ec002f791a5723cd
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-08-24 16:55:18 +0800

    Changes to perms and installs

--- setup/etc_files/mdp/init.sh
+++ setup/etc_files/mdp/init.sh
@@ -2,14 +2,22 @@
 
 
 PROJECT_ROOT_GIT="/home/pi/mdp-rpi.git"  # matches "~/mdp-rpi.git"
+PROJECT_ROOT_DIR="/home/pi/mdp-rpi"
 POST_REC_ORIGINAL_FILE="/etc/mdp/__post_receive.sh"
 
 create_git_repo() {
 
+  sudo apt install git -y
+  git config --global init.defaultBranch main
+
   # Create git folder if not exist
   if [ ! -r "$PROJECT_ROOT_GIT" ]; then
-    mkdir -p "$PROJECT_ROOT_GIT"
+    sudo mkdir -p "$PROJECT_ROOT_GIT"
+    sudo mkdir -p "$PROJECT_ROOT_DIR"
+    sudo chown -Rf pi:pi "$PROJECT_ROOT_GIT"
+    sudo chown -Rf pi:pi "$PROJECT_ROOT_DIR"
     echo "Created Git repo path! $PROJECT_ROOT_GIT"
+    echo "Created Git dir path! $PROJECT_ROOT_DIR"
   else
     echo "Git repo path already exists! $PROJECT_ROOT_GIT"
   fi
@@ -22,8 +30,8 @@ create_git_repo() {
 
       # Copy post-receive hook into the hooks directory
       if [ -f "$POST_REC_ORIGINAL_FILE" ]; then
-        cp "$POST_REC_ORIGINAL_FILE" "$PROJECT_ROOT_GIT/hooks/post-receive"
-        chmod +x "$PROJECT_ROOT_GIT/hooks/post-receive"
+        sudo cp "$POST_REC_ORIGINAL_FILE" "$PROJECT_ROOT_GIT/hooks/post-receive"
+        sudo chmod a+rx "$PROJECT_ROOT_GIT/hooks/post-receive"
         echo "Installed post-receive hook!"
       else
         echo "Post-receive hook script not found at $HOOK_SCRIPT_PATH"
@@ -32,11 +40,20 @@ create_git_repo() {
   else
     echo "$PROJECT_ROOT_GIT is already a git repo!"
   fi
+}
 
+install_global_deps() {
+
+  sudo apt install vim -y
+  sudo apt install python3.11 -y
+  sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1
 
 }
 
 
 
+
+
 # Execute functions
 create_git_repo
+install_global_deps
--- setup/etc_files/start_hotspot.sh
+++ setup/etc_files/start_hotspot.sh
@@ -1,11 +1,7 @@
 #!/bin/sh -e
 
-DNS_SERVERS=$(nmcli device show eth0 | grep IP4.DNS | awk '{print $2}')
-
 sudo nmcli device wifi hotspot ssid MDP_GROUP_38 password password
-nmcli connection modify Hotspot ipv4.dns "$DNS_SERVERS"
 sudo nmcli connection modify Hotspot ipv4.addresses 192.168.100.100/16
 sudo nmcli connection down Hotspot
 sudo nmcli connection up Hotspot
-
 
