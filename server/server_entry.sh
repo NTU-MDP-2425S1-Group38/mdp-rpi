fdf7550a4a1df5fab57ecc339afa2edaea2080bb
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-08-26 23:46:22 +0800

    Updated script ex

--- server/server_entry.sh
+++ server/server_entry.sh
@@ -3,3 +3,7 @@
 python3.11 -m venv /home/user/mdp-rpi/server/app/venv
 
 . /home/user/mdp-rpi/server/app/venv/bin/activate
+
+echo "Activated venv; starting server"
+
+python /home/user/mdp-rpi/server/app/main.py
