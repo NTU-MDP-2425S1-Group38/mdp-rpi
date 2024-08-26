149fcb8cf04d4c6628090dc4c80d585394c472b9
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-08-26 23:38:10 +0800

    Updated script ex

--- server/server_install_deps.sh
+++ server/server_install_deps.sh
@@ -3,7 +3,7 @@
 # shellcheck disable=SC2164
 cd /home/user/mdp-rpi/server/app
 
-python3 -m venv /home/user/mdp-rpi/server/app/venv
+python3.11 -m venv /home/user/mdp-rpi/server/app/venv
 
 # shellcheck disable=SC3046
 source /home/user/mdp-rpi/server/app/venv/bin/activate
