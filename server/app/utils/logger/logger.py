b9e4a70003dc8c20fa54e7ce528db783b86cae5a
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-08-26 21:56:04 +0800

    Updated logger to be more consistent in logging

--- server/app/utils/logger/logger.py
+++ server/app/utils/logger/logger.py
@@ -2,9 +2,29 @@ import logging
 import os
 from time import localtime, strftime
 
+from utils.metaclass.singleton import Singleton
+
+
+LOGGING_FILE_NAME = f"{os.path.dirname(__file__)}/../../logs/{strftime('%Y-%m-%d_%H-%M-%S', localtime())}.log"
+
+
 
 def init_logger() -> None:
+
+    # Formatting stuff
+    log_format = '[%(levelname)s][%(filename)s:%(lineno)s][%(asctime)s] %(message)s'
+    date_format = '%Y:%m:%d, %H:%M'
+
+    # Sets config for logging to file
     logging.basicConfig(
-        filename=f"{os.path.dirname(__file__)}/../../logs/{strftime('%Y-%m-%d_%H-%M-%S', localtime())}.log",
+        filename=LOGGING_FILE_NAME,
+        format=log_format,
+        datefmt=date_format,
         level=logging.DEBUG
     )
+
+    # Add StreamHandler to print logs to the console
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(logging.Formatter(log_format, date_format))
+    logging.getLogger().addHandler(console_handler)
