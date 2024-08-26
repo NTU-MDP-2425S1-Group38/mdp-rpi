1bbc66b3210789ad46204af505d2e6d63726fe26
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-08-26 21:56:14 +0800

    Improved singleton metaclass

--- server/app/utils/metaclass/singleton.py
+++ server/app/utils/metaclass/singleton.py
@@ -8,23 +8,26 @@ class Singleton(type):
     See original: https://stackoverflow.com/a/63483209
     ChatGPT thread/processing safe: https://chatgpt.com/share/185653f3-6d0b-4772-b997-42335c2fee0f
     """
-    _process_lock = multiprocessing.Lock()  # A lock for synchronizing across processes
+    _process_lock = multiprocessing.Lock()
 
     def __init__(cls, *args, **kwargs):
         cls.__instance = None
-        cls._manager = multiprocessing.Manager()  # Manager to create shared objects
-        cls._shared_dict = cls._manager.dict()  # Shared dictionary across processes
-        cls._thread_lock = threading.Lock()  # A lock for synchronizing threads within a process
+        cls._thread_lock = threading.Lock()
         super().__init__(*args, **kwargs)
 
+    def _get_or_create_shared_resources(cls):
+        if not hasattr(cls, '_manager'):
+            cls._manager = multiprocessing.Manager()
+            cls._shared_dict = cls._manager.dict()
+        return cls._shared_dict
+
     def __call__(cls, *args, **kwargs):
-        # Lock for multiprocessing
         with cls._process_lock:
-            # Lock for multithreading within a process
             with cls._thread_lock:
-                if cls.__instance is None and 'instance' not in cls._shared_dict:
+                shared_dict = cls._get_or_create_shared_resources()
+                if cls.__instance is None and 'instance' not in shared_dict:
                     cls.__instance = super().__call__(*args, **kwargs)
-                    cls._shared_dict['instance'] = cls.__instance
+                    shared_dict['instance'] = cls.__instance
                 else:
-                    cls.__instance = cls._shared_dict['instance']
+                    cls.__instance = shared_dict['instance']
         return cls.__instance
