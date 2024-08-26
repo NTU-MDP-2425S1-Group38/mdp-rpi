59091b52f5977fd7b7ccf67ecacb3521f42d4f75
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-08-26 12:55:12 +0800

    Added initialisation to bluetooth

--- setup/etc_files/mdp/init.sh
+++ setup/etc_files/mdp/init.sh
@@ -44,11 +44,19 @@ create_git_repo() {
 }
 
 install_global_deps() {
-
+  echo "Installing vim"
   sudo apt install vim -y
+  echo "Installing Python 3.11"
   sudo apt install python3.11 -y
   sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1
 
+}
+
+setup_bluetooth() {
+  echo "Setting up bluetooth configurations"
+  sudo sed -i 's/ExecStart=\/usr\/libexec\/bluetooth\/bluetoothd/ExecStart=\/usr\/libexec\/bluetooth\/bluetoothd -C --noplugin=sap \nExecStartPost=\/usr\/bin\/sdptool add SP/' /lib/systemd/system/bluetooth.service
+  sudo systemctl daemon-reload
+  sudo systemctl restart bluetooth
 }
 
 
@@ -58,3 +66,4 @@ install_global_deps() {
 # Execute functions
 create_git_repo
 install_global_deps
+setup_bluetooth
