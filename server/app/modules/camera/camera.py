9e799942e43134028a08cec088012898c6e0ce27
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-09-06 20:56:18 +0800

    Updated camera to return base64 image

--- server/app/modules/camera/camera.py
+++ server/app/modules/camera/camera.py
@@ -1,10 +1,9 @@
-import cv2
-import numpy as np
-from utils.metaclass.singleton import Singleton
+import base64
+import io
 import picamera2
 
 
-class Camera():
+class Camera:
     """
     This class handles all camera interaction.
     Basically just reads from the camera (hopefully)
@@ -21,10 +20,23 @@ class Camera():
         self.cam.configure(config)
         self.cam.start()
 
-    def capture(self) -> np.array:
+
+    def capture(self) -> str:
         """
         Method to read the image of the camera as a np array
-        :return:
+        :return: Base64 image
         """
-        frame = self.cam.capture_array()
-        return frame
+
+        # Create a BytesIO object to store the image in memory
+        image_stream = io.BytesIO()
+
+        # Capture the image in JPEG format with quality 40 (0.4)
+        self.cam.capture(image_stream, format='jpeg', quality=40)
+
+        # Move the pointer to the beginning of the BytesIO buffer
+        image_stream.seek(0)
+
+        # Convert the BytesIO buffer to base64
+        image_base64 = base64.b64encode(image_stream.getvalue()).decode('utf-8')
+
+        return image_base64
