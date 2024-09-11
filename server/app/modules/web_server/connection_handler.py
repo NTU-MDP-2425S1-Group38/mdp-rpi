5af9cedcb33f71a4382263d7298c651af562cbe5
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-09-11 23:49:02 +0800

    Removed reflection of observer res

--- server/app/modules/web_server/connection_handler.py
+++ server/app/modules/web_server/connection_handler.py
@@ -13,7 +13,7 @@ async def connection_handler(websocket: WebSocket):
     try:
         while True:
             data = await websocket.receive_text()
-            await websocket.send_text(data)
+            # await websocket.send_text(data)
     except WebSocketDisconnect:
         ConnectionManager().remove_connection(websocket)
 
