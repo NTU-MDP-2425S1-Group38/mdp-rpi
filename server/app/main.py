e915134a27b88b8d292772e1046604186f9f00e1
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-09-11 15:57:33 +0800

    Testing only the camera

--- server/app/main.py
+++ server/app/main.py
@@ -50,138 +50,41 @@ def main():
     load_dotenv()
     init_logger()
 
-    gamestate = GameState()
+    # gamestate = GameState()
+    #
+    # processes = []
+    #
+    # def signal_handler(signum, frame):
+    #     logging.getLogger().info("Received termination signal. Shutting down gracefully...")
+    #     for process in processes:
+    #         process.kill()
+    #
+    #     sys.exit(0)
+    #
+    # # Register the signal handler for SIGINT and SIGTERM
+    # signal.signal(signal.SIGINT, signal_handler)
+    # signal.signal(signal.SIGTERM, signal_handler)
+    #
+    # server_process = Process(target=run_web_server)
+    # bluetooth_process = Process(target=run_bluetooth_server)
+    # stm_process = Process(target=run_stm)
+    #
+    # processes.extend([
+    #     server_process,
+    #     bluetooth_process,
+    #     stm_process
+    # ])
+    #
+    # for p in processes:
+    #     p.start()
+    #
+    # for p in processes:
+    #     p.join()
 
-    processes = []
+    cam = Camera()
+    while True:
+        print(cam.capture())
 
-    def signal_handler(signum, frame):
-        logging.getLogger().info("Received termination signal. Shutting down gracefully...")
-        for process in processes:
-            process.kill()
-
-        sys.exit(0)
-
-    # Register the signal handler for SIGINT and SIGTERM
-    signal.signal(signal.SIGINT, signal_handler)
-    signal.signal(signal.SIGTERM, signal_handler)
-
-    server_process = Process(target=run_web_server)
-    bluetooth_process = Process(target=run_bluetooth_server)
-    stm_process = Process(target=run_stm)
-
-    processes.extend([
-        server_process,
-        bluetooth_process,
-        stm_process
-    ])
-
-    for p in processes:
-        p.start()
-
-    for p in processes:
-        p.join()
-
-    # drive_speed = 40
-    # drive_angle = 25
-
-    # stm = STM()
-
-    # stm.connect()
-
-    # Forward
-    # stm.send_cmd("T", drive_speed, 0, 30)
-    # print("Forward")
-
-    # Backward
-    # stm.send_cmd("t", drive_speed, 0, 30)
-    # print("Backward")
-
-    # Forward Left
-    # stm.send_cmd("T", drive_speed, -drive_angle, 90)
-    # print("Forward Left")
-
-    # Forward Right
-    # stm.send_cmd("T", drive_speed, drive_angle, 90)
-    # print("Forward Right")
-
-    # Backward Left
-    # stm.send_cmd("t", drive_speed, -drive_angle,90)
-    # print("Backward Left")
-
-    # Backward Right
-    # stm.send_cmd("t", drive_speed, drive_angle, 90)
-    # print("Backward Right")
-
-# def stm_receive(self) -> None:
-#     msg = ""
-#     while True:
-#         message_rcv = None
-#         try:
-#             message_rcv = self.stm.wait_receive()
-#             print("Message received from STM: ", message_rcv)
-#             if "fS" in message_rcv:
-#                 self.set_stm_stop(
-#                     True
-#                 )  # Finished stopping, can start delay to recognise image
-#                 print("Setting STM Stopped to true")
-#             elif message_rcv[0] == "f":
-#                 # Finished command, send to android
-#                 message_split = message_rcv[1:].split(
-#                     "|"
-#                 )  # Ignore the 'f' at the start
-#                 cmd_speed = message_split[0]
-#                 turning_degree = message_split[1]
-#                 distance = message_split[2].strip()
-#
-#                 cmd = cmd_speed[0]  # Command (t/T)
-#                 speed = cmd_speed[1:]
-#
-#                 send_count = 1
-#
-#                 if turning_degree == f"-{self.drive_angle}":
-#                     # Turn left
-#                     if cmd == "t":
-#                         # Backward left
-#                         msg = "TURN,BACKWARD_LEFT,0"
-#                     elif cmd == "T":
-#                         # Forward left
-#                         msg = "TURN,FORWARD_LEFT,0"
-#                 elif turning_degree == f"{self.drive_angle}":
-#                     # Turn right
-#                     if cmd == "t":
-#                         # Backward right
-#                         msg = "TURN,BACKWARD_RIGHT,0"
-#                     elif cmd == "T":
-#                         # Forward right
-#                         msg = "TURN,FORWARD_RIGHT,0"
-#                 elif turning_degree == "0":
-#                     if cmd == "t":
-#                         # Backward
-#                         msg = "MOVE,BACKWARD," + distance
-#                     elif cmd == "T":
-#                         # Forward
-#                         msg = "MOVE,FORWARD," + distance
-#                 else:
-#                     # Unknown turning degree
-#                     print("Unknown turning degree")
-#                     msg = "No instruction"
-#                     continue
-#
-#                 print("Msg: ", msg)
-#                 try:
-#                     self.android.send(msg)
-#                     print("SENT TO ANDROID SUCCESSFULLY: ", msg)
-#                 except OSError:
-#                     self.android_dropped.set()
-#                     print("Event set: Android dropped")
-#
-#                 self.android_dropped.clear()  # Clear previously set event
-#
-#         except OSError as e:
-#             print(f"Error in receiving STM data: {e}")
-#
-#         if message_rcv is None:
-#             continue
 
 
 if __name__ == "__main__":
