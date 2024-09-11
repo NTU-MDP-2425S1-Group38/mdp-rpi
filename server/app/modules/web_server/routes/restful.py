c23a5c4a174c106b0fdc20a5ad249d89d296a345
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-09-11 14:22:02 +0800

    updated logging and return for capture command

--- server/app/modules/web_server/routes/restful.py
+++ server/app/modules/web_server/routes/restful.py
@@ -6,10 +6,12 @@ from fastapi import APIRouter
 from modules.gamestate.gamestate import GameState
 
 rest_endpoints = APIRouter()
+logger = logging.getLogger("/command/capture")
 
 
 @rest_endpoints.get("/")
 async def index():
+    logger.info("Return base route")
     return f"Hello the time now is {strftime('%Y-%m-%d_%H-%M-%S', localtime())}"
 
 
@@ -20,6 +22,6 @@ async def capture():
     Endpoint to trigger the GameState class to take a picture and send to slaves
     :return:
     """
-    logger = logging.getLogger("/command/capture")
     logger.info("Received capture command")
-    GameState().capture_and_process_image()
+    state = GameState().capture_and_process_image()
+    return state
