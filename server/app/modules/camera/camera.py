d5f7a757df4aaf9373ce9a8a32e80b1e953bf87b
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-09-11 19:16:03 +0800

    Test image capturing as np arr

--- server/app/modules/camera/camera.py
+++ server/app/modules/camera/camera.py
@@ -8,7 +8,7 @@ import picamera2
 from utils.metaclass.singleton import Singleton
 
 
-class Camera():
+class Camera:
     """
     This class handles all camera interaction.
     Basically just reads from the camera (hopefully)
@@ -36,24 +36,24 @@ class Camera():
         :return: Base64 image
         """
 
-        self.logger.info("Attempting to acquire lock for capturing image!")
-
         self.logger.info("Capturing image!")
 
         # Create a BytesIO object to store the image in memory
         image_stream = io.BytesIO()
 
+        self.logger.info("Created bytes array")
+
         # Capture the image in JPEG format
-        self.cam.capture_file(image_stream, format='jpeg')
+        print(self.cam.capture_array())
 
-        self.logger.info("Image has been captured as bytes!")
+        self.logger.info("Image has been captured as np.Array!")
 
-        # Move the pointer to the beginning of the BytesIO buffer
-        image_stream.seek(0)
-
-        # Convert the BytesIO buffer to base64
-        image_base64 = base64.b64encode(image_stream.getvalue()).decode('utf-8')
-
-        self.logger.info("Image has been captured as base64!")
-
-        return image_base64
+        # # Move the pointer to the beginning of the BytesIO buffer
+        # image_stream.seek(0)
+        #
+        # # Convert the BytesIO buffer to base64
+        # image_base64 = base64.b64encode(image_stream.getvalue()).decode('utf-8')
+        #
+        # self.logger.info("Image has been captured as base64!")
+        #
+        # return image_base64
