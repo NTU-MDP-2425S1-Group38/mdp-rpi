22bb78aff953f69402a495ed2999778654becb9d
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-09-11 15:04:49 +0800

    Added script to update deps

--- server/server_install_deps.sh
+++ server/server_install_deps.sh
@@ -4,4 +4,11 @@ python3.11 -m venv --system-site-packages /home/user/mdp-rpi/server/app/venv
 
 . /home/user/mdp-rpi/server/app/venv/bin/activate
 
+sudo apt update
+sudo apt install python3-dev python3-pip build-essential libbluetooth-dev -y
+
 pip install -r /home/user/mdp-rpi/server/app/requirements.txt
+
+sudo apt install -y python3-libcamera python3-kms++
+sudo apt install -y python3-prctl libatlas-base-dev ffmpeg libopenjp2-7
+sudo apt install -y python3-picamera2
