2c7abc613c5fd5b2334d749c488b693bd61999ed
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-09-12 15:24:22 +0800

    Created validation for obstacle ID on generation

--- server/app/app_types/obstacle.py
+++ server/app/app_types/obstacle.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from pydantic import BaseModel, Field, model_serializer
+from pydantic import BaseModel, Field, model_serializer, model_validator
 from app_types.primatives.obstacle_direction import ObstacleDirection
 from app_types.primatives.obstacle_label import ObstacleLabel
 from app_types.primatives.position import Position
@@ -12,10 +12,20 @@ class Obstacle(BaseModel):
     label: ObstacleLabel = Field(default=ObstacleLabel.Unknown)
     visited: bool = Field(default=False)
 
+    # Dynamically generate the 'id' during instantiation
+    @classmethod
+    @model_validator(mode="before")
+    def generate_id(cls, values):
+        position = values.get("position")
+        direction = values.get("direction")
+        if (position and direction is not None) and values.get["id"] is not None:
+            values["id"] = 10_000 * position.x + 100 * position.y + direction
+        return values
+
     @model_serializer()
     def _serial(self):
         return {
-            "id": 1000 * self.position.x + 100 * self.position.y + self.direction,
+            "id": self.id,
             "x": self.position.x,
             "y": self.position.y,
             "d": self.direction,
