331a6e32fe98605a53e076f14e873ed907307a33
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-09-12 15:24:49 +0800

    Added logging and algo parsing in commad_handler

--- server/app/modules/web_server/connection_handler.py
+++ server/app/modules/web_server/connection_handler.py
@@ -3,6 +3,7 @@ import logging
 from fastapi import WebSocket, WebSocketDisconnect
 
 from app_types.primatives.cv import CvResponse
+from app_types.primatives.command import AlgoCommandResponse
 from modules.web_server.connection_manager import ConnectionManager
 
 
@@ -19,8 +20,13 @@ async def connection_handler(websocket: WebSocket):
             logger.info(f"Received: {data}")
             
             if "label" in data.keys():
+                logger.info("Parsing data as CvResponse")
                 cvRes = CvResponse.model_validate(data)
                 ConnectionManager().handle_cv_response_callback(cvRes)
+            if "commands" in data.keys():
+                logger.info("Parsing data as AlgoCommandResponse")
+                algoRes = AlgoCommandResponse.model_validate(data)
+                ConnectionManager().handle_algo_response_callback(algoRes)
 
             # await websocket.send_text(data)
     except WebSocketDisconnect:
