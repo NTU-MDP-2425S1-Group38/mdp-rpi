15037154c3bc736915d141b0d75a4136c1561f4c
zonpig <limbryan00@gmail.com>
2024-09-14 17:01:26 +0800

    Added task 2 code

--- server/app/modules/gamestate/gamestate.py
+++ server/app/modules/gamestate/gamestate.py
@@ -46,10 +46,60 @@ class GameState(metaclass=Singleton):
         self.logger.info("Initialising connection manager")
         self.connection_manager = ConnectionManager()
 
-        self.STM_Stopped = False
-
         # self.logger.info("Initialising STM connector")
         # self.stm = STM()
+
+        # Variables Related to Task 1
+        self.last_image = None
+        self.STM_Stopped = False
+
+        self.start_time = 0
+        self.drive_speed = 55
+        self.drive_angle = 25
+
+        # Variables Related to Task 2
+        self.num_M = 0
+        self.num_obstacle = 1
+        self.is_right1 = False  # track whether first obstacle was a left or right turn
+        self.is_right2 = False  # track whether second obstacle was a left or right turn
+        self.done_obstacle2 = False  # track whether second arrow was seen
+
+        self.on_arrow_callback = (
+            None  # callback that takes in a single argument, boolean is_right
+        )
+
+        self.capture_dist1 = 30  # distance between first arrow and where car will stop.
+        self.capture_dist2 = (
+            20  # distance between second arrow and where car will stop.
+        )
+        self.obstacle_dist1 = None  # distance between carpark and first obstacle.
+        self.obstacle_dist2 = None  # distance between carpark and second obstacle.
+        self.wall_dist = None  # distance driven to face wall after second obstacle.
+        self.wall_complete = False  # signal wall has been tracked.
+        self.obstacle2_length_half = None  # length of obstacle.
+
+        self.turning_r = 40  # turning radius
+        self.r0 = 21  # absolute distance from center line after passing obstacle 1
+        self.chassis_cm = 15  # length from axle to axle
+        self.wheelbase_cm = 16.5  # length between front wheels
+
+        # tune to balance speed with precision. kachow!
+        if self.is_outdoors:
+            self.theta2 = 10  # angle to face second obstacle after first arc.
+            self.drive_speed = 35
+            self.obstacle_speed = 45
+            self.wall_track_speed = 35
+            self.carpark_speed = 45
+        else:
+            self.theta2 = 10  # angle to face second obstacle after first arc.
+            self.drive_speed = 45
+            self.obstacle_speed = 50
+            self.wall_track_speed = 45
+            self.carpark_speed = 30
+
+        # left and right arrow IDs
+        self.LEFT_ARROW_ID = "39"
+        self.RIGHT_ARROW_ID = "38"
 
     """
     CV methods
@@ -247,15 +297,211 @@ class GameState(metaclass=Singleton):
 
         self.stop()
 
+    def _stop_task_one(self):
+        """Stops all processes on the RPi and disconnects from Android, STM and PC"""
+        time.sleep(0.2)
+        self.android.send("STOP")
+        print("Program Ended\n")
+
     """
     Methods related to task 2.
     """
 
-    def sin_deg(angle):
-        return math.sin(angle * math.pi / 180)
+    def send_M(self):
+        self.stm.send_cmd("M", 0, 0, 0)
 
-    def cos_deg(angle):
-        return math.cos(angle * math.pi / 180)
+    def send_D(self):
+        self.stm.send_cmd("D", 0, 0, 0)
+
+    def drive(self, angle, val, is_forward=True, speed=None):
+        if speed is None:
+            speed = self.drive_speed
+
+        if val < 0:
+            val = -val
+            is_forward = not is_forward
+
+        self.stm.send_cmd("T" if is_forward else "t", speed, angle, val)
+
+    def drive_until(self, angle, val, is_forward=True, speed=None):
+        if speed is None:
+            speed = self.drive_speed
+
+        self.stm.send_cmd("W" if is_forward else "w", speed, angle, val)
+
+    def wall_ride(self, angle, is_right, threshold=30, is_forward=True, speed=None):
+        if speed is None:
+            speed = self.drive_speed
+
+        char = "R" if is_right else "L"
+        if not is_forward:
+            char = char.lower()
+
+        self.stm.send_cmd(char, speed, angle, threshold)
+
+    def calc_arc(self, x, y):
+        is_right = x >= 0
+        if not is_right:
+            x = -x
+
+        r = (x**2 + y**2) / (2 * x)  # turning radius to execute.
+        angle = (
+            math.atan(self.chassis_cm / (r - self.wheelbase_cm / 2)) * 180 / math.pi
+        )  # calculate steering angle.
+        theta = (
+            math.atan(y / (r - x)) * 180 / math.pi
+        )  # resultant facing angle after turn.
+        if angle > 25:
+            angle = 25
+        if not is_right:
+            angle = -angle
+
+        return angle, theta
+
+    # drive towards obstacle (and insert 'D' to signal distance tracking).
+    def perform_toward_obstacle(self, capture_dist=30) -> None:
+        # self.stm.send_cmd("W", self.drive_speed, 0, 50)
+        self.send_D()
+        self.drive_until(0, capture_dist, speed=self.obstacle_speed)
+        self.send_D()
+
+    # drive until IR sensor is above threshold (and insert 'D' for distance tracking).
+    def perform_wall_track(
+        self, is_right, is_forward=True, threshold=30, should_track=False
+    ) -> None:
+        if should_track:
+            self.send_D()
+
+        self.wall_ride(
+            0,
+            is_right,
+            is_forward=is_forward,
+            threshold=threshold,
+            speed=self.wall_track_speed,
+        )
+        if should_track:
+            self.send_D()
+
+    # drive arc for first 10x10 obstacle.
+    def perform_arc1(self, is_right) -> None:
+        # mark image seen.
+
+        # get initial turning angle.
+        angle = 25 if is_right else -25
+
+        turn_theta = 33
+        self.drive(angle, turn_theta)
+        self.drive(0, 18)
+        self.drive(-angle, turn_theta + self.theta2)
+        self.send_M()
+        self.set_last_image(None)
+
+    # drive arc for second 60x10 obstacle.
+    def perform_arc2(self, is_right1, is_right2) -> None:
+        is_cross = is_right1 != is_right2
+        angle = 25 if is_right2 else -25
+
+        # while self.obstacle_dist2 is None:
+        #     pass
+
+        # turn to be parallel to the second obstacle.
+        if is_cross:
+            gamma = 25
+            self.drive(0, 10)
+            self.drive(-angle, gamma, False)
+            self.drive(angle, 90 - gamma - self.theta2)
+        else:
+            gamma = 37
+            delta = self.theta2 * 1.7
+            self.drive(angle, self.theta2 + delta)
+            self.drive(-angle, gamma, False)
+            self.drive(angle * 0.8, 90 - gamma - delta)
+
+        # use wall tracking to find wall.
+        wall_is_right = not is_right2
+        self.perform_wall_track(wall_is_right, is_forward=False, threshold=-50)
+        self.perform_wall_track(wall_is_right, is_forward=True, threshold=50)
+
+        self.stm.send_cmd("S", 0, 0, 0)  # Command to stop the Robot
+
+        self.drive(-angle, 180)
+        self.perform_wall_track(
+            wall_is_right, is_forward=True, threshold=50, should_track=True
+        )
+        # wait for wall distance to be calculated.
+        while not self.wall_complete:
+            pass
+
+        print(f"----------------- Wall distance: {self.wall_dist:.3f}cm --------------")
+        self.drive(-angle, 90)
+
+    # sends arrow message to android.
+    def send_arrow_android(self, obstacle_num, is_right):
+        self.android.send(f"ARROW,{obstacle_num},{'R' if is_right else 'L'}")
+
+    # set this callback it is time to detect an arrow for obstacle 1.
+    def callback_obstacle1(self, is_right) -> None:
+        self.send_arrow_android(1, is_right)
+        with self.lock:
+            self.is_right1 = is_right
+
+        self.perform_arc1(is_right)
+        self.perform_toward_obstacle()
+
+        self.on_arrow_callback = None  # clear callback.
+
+    # set this callback it is time to detect an arrow for obstacle 2.
+    def callback_obstacle2(self, is_right) -> None:
+        self.send_arrow_android(2, is_right)
+        with self.lock:
+            self.done_obstacle2 = True
+            self.is_right2 = is_right
+
+        self.perform_arc2(self.is_right1, is_right)
+        self.on_arrow_callback = None  # clear callback.
+
+        self.perform_carpark()
+
+    # drive back to the carpark.
+    def perform_carpark(self) -> None:
+        while (
+            self.obstacle_dist1 is None
+            or self.obstacle_dist2 is None
+            or not self.wall_complete
+        ):
+            pass
+
+        angle = -25
+        if self.is_right2:
+            angle = 25
+
+        y1 = (self.obstacle_dist2 + self.chassis_cm + self.capture_dist2) * math.cos(
+            self.theta2 * math.pi / 180
+        )
+        y2 = self.obstacle_dist1 + self.chassis_cm / 2 + self.capture_dist1
+
+        print(f"y1: {y1}, y2: {y2}")
+        d1 = 0.7 * y1
+        self.drive(0, d1)
+        a, d = self.calc_arc(
+            self.wall_dist / 2 + self.wheelbase_cm, y1 - d1 + y2 - self.turning_r * 0.25
+        )
+        print(f"a: {a}, d: {d}")
+        self.drive(-a if self.is_right2 else a, d)
+        # self.drive(-angle, 90 - d)
+
+        gamma = 30
+        self.drive(angle, gamma, is_forward=False)
+        self.drive(-angle, 90 - gamma - d)
+
+        self.wall_ride(0, self.is_right2, is_forward=False, threshold=-45)
+        self.wall_ride(0, self.is_right2, threshold=45)
+        # self.drive(angle, d)
+        self.drive(angle, 90)
+        self.drive_until(
+            0, 15, speed=self.carpark_speed
+        )  # slowly advance into carpark.
+        self.send_M()  # signal stop.
 
     def _run_task_two(self, config) -> None:
         """
@@ -263,48 +509,22 @@ class GameState(metaclass=Singleton):
         Obstacles need to be set and instructions need to be added prior to running.
         :return: None
         """
-        num_M = 0
-        num_obstacle = 1
-        is_right1 = False  # track whether first obstacle was a left or right turn
-        is_right2 = False  # track whether second obstacle was a left or right turn
-        done_obstacle2 = False  # track whether second arrow was seen
 
-        on_arrow_callback = (
-            None  # callback that takes in a single argument, boolean is_right
-        )
-
-        capture_dist1 = 30  # distance between first arrow and where car will stop.
-        capture_dist2 = 20  # distance between second arrow and where car will stop.
-        obstacle_dist1 = None  # distance between carpark and first obstacle.
-        obstacle_dist2 = None  # distance between carpark and second obstacle.
-        wall_dist = None  # distance driven to face wall after second obstacle.
-        wall_complete = False  # signal wall has been tracked.
-        obstacle2_length_half = None  # length of obstacle.
-
-        turning_r = 40  # turning radius
-        r0 = 21  # absolute distance from center line after passing obstacle 1
-        chassis_cm = 15  # length from axle to axle
-        wheelbase_cm = 16.5  # length between front wheels
-
-        # tune to balance speed with precision. kachow!
-        if config.is_outdoors:
-            theta2 = 10  # angle to face second obstacle after first arc.
-            drive_speed = 35
-            obstacle_speed = 45
-            wall_track_speed = 35
-            carpark_speed = 45
-        else:
-            theta2 = 10  # angle to face second obstacle after first arc.
-            drive_speed = 45
-            obstacle_speed = 50
-            wall_track_speed = 45
-            carpark_speed = 30
-
-        # left and right arrow IDs
-        self.LEFT_ARROW_ID = "39"
-        self.RIGHT_ARROW_ID = "38"
+        self.logger.info("Starting program...")
+        self.logger.info("Sending initial commands to the STM32...")
+        self.start_time = time.time_ns()
+        self.perform_toward_obstacle(self.capture_dist1)
 
         self.logger.info("Run Task Two completed!")
+
+    def _stop_task_two(self):
+        """Stops all processes on the RPi and disconnects from Android, STM and PC"""
+        self.android.send("STOP")  # stop the Android tablet.
+        self.pc.send("STITCH")  # request a stitch.
+
+        print(
+            f">>>>>>>>>> Ended: {(time.time_ns() - self.start_time) / 1e9:.3f}s. -------------- !"
+        )
 
     """
     Main entry methods
@@ -318,13 +538,5 @@ class GameState(metaclass=Singleton):
         """
         if task == 1:
             self._run_task_one()
-
-    def stop(self):
-        """Stops all processes on the RPi and disconnects from Android, STM and PC"""
-        time.sleep(0.2)
-        self.android.send("STOP")
-        # self.android.disconnect()
-        # self.stm.disconnect()
-        # self.pc.disconnect()
-        # TODO: Add Stream disconnect/end
-        print("Program Ended\n")
+        elif task == 2:
+            self._run_task_two()
--- server/app/modules/serial/stm32.py
+++ server/app/modules/serial/stm32.py
@@ -3,6 +3,7 @@ from .configuration import BAUD_RATE, SERIAL_PORT
 from pathlib import Path
 from typing import Optional
 from modules.gamestate.gamestate import GameState
+import time
 
 # from modules.serial.android import Android
 
@@ -133,66 +134,85 @@ class STM(metaclass=Singleton):
     def run_task_2(self):
         """Run the STM32 module."""
         self.connect()
-        msg = ""
 
         while True:
-            message_rcv = None
-
             try:
-                message_rcv = self.wait_receive()
-                print("Message received from STM: ", message_rcv)
-                if message_rcv[0] == "f":
-                    # Finished command, send to android
-                    message_split = message_rcv[1:].split(
-                        "|"
-                    )  # Ignore the 'f' at the start
-                    cmd_speed = message_split[0]
-                    turning_degree = message_split[1]
-                    distance = message_split[2].strip()
-
-                    cmd = cmd_speed[0]  # Command (t/T)
-
-                    if turning_degree == f"-{self.drive_angle}":
-                        # Turn left
-                        if cmd == "t":
-                            # Backward left
-                            msg = "TURN,BACKWARD_LEFT,0"
-                        elif cmd == "T":
-                            # Forward left
-                            msg = "TURN,FORWARD_LEFT,0"
-                    elif turning_degree == f"{self.drive_angle}":
-                        # Turn right
-                        if cmd == "t":
-                            # Backward right
-                            msg = "TURN,BACKWARD_RIGHT,0"
-                        elif cmd == "T":
-                            # Forward right
-                            msg = "TURN,FORWARD_RIGHT,0"
-                    elif turning_degree == "0":
-                        if cmd == "t":
-                            # Backward
-                            msg = "MOVE,BACKWARD," + distance
-                        elif cmd == "T":
-                            # Forward
-                            msg = "MOVE,FORWARD," + distance
-                    else:
-                        # Unknown turning degree
-                        print("Unknown turning degree")
-                        msg = "No instruction"
+                messages = self.stm.wait_receive()
+                print(f"all messages: {messages}")
+                for message_rcv in messages.split("\n"):
+                    if len(message_rcv) == 0:
                         continue
+                    print("Message received from STM: ", message_rcv)
+                    if "M" in message_rcv:
+                        if self.gamestate.num_M == 0:
+                            time.sleep(0.25)
+                            self.pc.send("SEEN")
+                        elif self.gamestate.num_M == 1:
+                            self.gamestate.stop()
 
-                    print("Msg: ", msg)
-                    try:
-                        self.android.send(msg)
-                        print("SENT TO ANDROID SUCCESSFULLY: ", msg)
-                    except OSError:
-                        self.android_dropped.set()
-                        print("Event set: Android dropped")
+                        self.num_M += 1
+                    elif "D" in message_rcv:
+                        dist_val_str = (
+                            message_rcv.replace("fD", "").replace("\0", "").strip()
+                        )
+                        if len(dist_val_str) == 0:
+                            # Robot is beginning drive towards obstacle, take in latest_image then decide what to do
+                            if self.gamestate.num_obstacle == 1:  # First Obstacle
+                                if (
+                                    self.gamestate.get_last_image()
+                                    == self.gamestate.RIGHT_ARROW_ID
+                                ):  # RIGHT ARROW
+                                    print("Right arrow detected")
+                                    self.gamestate.callback_obstacle1(True)
 
-                    self.android_dropped.clear()  # Clear previously set event
+                                elif (
+                                    self.gamestate.get_last_image()
+                                    == self.gamestate.LEFT_ARROW_ID
+                                ):  # LEFT ARROW
+                                    print("Left arrow detected")
+                                    self.gamestate.callback_obstacle1(False)
+
+                                else:
+                                    # set to trigger on next arrow found.
+                                    self.gamestate.on_arrow_callback = (
+                                        self.gamestate.callback_obstacle1
+                                    )
+
+                            elif self.gamestate.num_obstacle == 2:  # Second Obstacle
+                                if (
+                                    self.gamestate.get_last_image()
+                                    == self.gamestate.RIGHT_ARROW_ID
+                                ):  # RIGHT ARROW
+                                    print("Right arrow detected")
+                                    self.gamestate.callback_obstacle2(True)
+
+                                elif (
+                                    self.gamestate.get_last_image()
+                                    == self.gamestate.LEFT_ARROW_ID
+                                ):  # LEFT ARROW
+                                    print("Left arrow detected")
+                                    self.gamestate.callback_obstacle2(False)
+
+                                else:
+                                    # set to trigger on next arrow found.
+                                    self.gamestate.on_arrow_callback = (
+                                        self.gamestate.callback_obstacle2
+                                    )
+
+                            self.gamestate.num_obstacle += 1
+                        else:
+                            # Robot has finished tracking distance; save accordingly
+                            dist_val = float(dist_val_str)
+
+                            # set distances in order.
+                            with self.gamestate.lock:
+                                if self.gamestate.obstacle_dist1 is None:
+                                    self.gamestate.obstacle_dist1 = dist_val
+                                elif self.gamestate.obstacle_dist2 is None:
+                                    self.gamestate.obstacle_dist2 = dist_val
+                                elif self.gamestate.wall_dist is None:
+                                    self.gamestate.wall_dist = dist_val + 22.5
+                                    self.gamestate.wall_complete = True
 
             except OSError as e:
                 print(f"Error in receiving STM data: {e}")
-
-            if message_rcv is None:
-                continue
