a98436fc33368da1ba86ab2214aef9971631cd75
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-10-04 13:43:41 +0800

    Added entrypoint for task2

---
+++ server/server_entry_task_2.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+python3.11 -m venv --system-site-packages /home/user/mdp-rpi/server/app/venv
+
+. /home/user/mdp-rpi/server/app/venv/bin/activate
+
+echo "Activated venv; starting server"
+
+
+echo "Listening for USB ports"
+
+# List all active USB ports
+usb_ports=$(ls /dev/ttyUSB* /dev/ttyACM* 2>/dev/null)
+
+# Check if any USB ports are found
+if [ -n "$usb_ports" ]; then
+  # Change permissions for each found USB port
+  for port in $usb_ports; do
+    sudo chmod a+rw "$port"
+  done
+else
+  echo "No active USB ports found."
+fi
+
+python /home/user/mdp-rpi/server/app/main.py
