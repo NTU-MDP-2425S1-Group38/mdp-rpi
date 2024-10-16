0fdee2f36fe04428638aaa5451264620271e8e9d
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-10-16 18:31:17 +0800

    Fixxed offset

--- server/app/modules/tasks/task_two.py
+++ server/app/modules/tasks/task_two.py
@@ -327,7 +327,7 @@ class TaskTwoRunner(metaclass=Singleton):
         toggle_flip = 1 if arrow_direction == "left" else -1
 
         offset_distance = max(
-            int((2 ** 0.5) * ((self.config.OBSTACLE_WIDTH - 15)/ 2)),
+            int((2 ** 0.5) * ((self.config.OBSTACLE_WIDTH - 25)/ 2)),
             0
         )
         backtrack_distance = self.distance_to_backtrack - 30  # distance before making first 45deg turn
