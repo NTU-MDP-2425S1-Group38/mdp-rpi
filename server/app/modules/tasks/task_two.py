bcf4c72c947ecc9c216c825af5c3d68a7680f0fd
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-10-16 14:40:18 +0800

    Reduce turn speed

--- server/app/modules/tasks/task_two.py
+++ server/app/modules/tasks/task_two.py
@@ -29,7 +29,7 @@ class TaskTwoRunner(metaclass=Singleton):
 
     class ConfigManeuver:
         forward_speed: int = 80
-        turn_speed: int = 60
+        turn_speed: int = 40
 
         SERVO_TURN_ANGLE = 25
         BYPASS_DISTANCE: int
