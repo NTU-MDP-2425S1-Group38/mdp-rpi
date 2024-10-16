c0a1249c8acb9ae683c7c19a2ab35ac9ae2ff186
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-10-16 19:17:25 +0800

    Fixed offset

--- server/app/modules/tasks/task_two.py
+++ server/app/modules/tasks/task_two.py
@@ -334,7 +334,7 @@ class TaskTwoRunner(metaclass=Singleton):
         )
 
         # distance before making first 45deg turn
-        backtrack_distance = self.distance_to_backtrack - 7 - offset_distance
+        backtrack_distance = self.distance_to_backtrack - offset_distance
 
         self._log_tracked_distances("Performing backtrack")
 
